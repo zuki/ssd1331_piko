b5a9b3fda8235d99db45883cf90d11e10252a939
Keiji Suzuki <zuki.ebetsu@gmail.com>
2024-02-11 11:01:42 +0900

    色と線を正常に描画

---
+++ ssd1331.c
@@ -0,0 +1,227 @@
+/**
+ * Copyright (c) 2023 SUZUKI Keiji.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "pico/stdlib.h"
+#include "pico/binary_info.h"
+#include "hardware/spi.h"
+#include "ssd1331.h"
+
+/* SPI経由で96x64 16bit-Color OLEDディスプレイを駆動するSSD1331を
+   操作するサンプルコード
+
+   * GPIO 17 (pin 22) Chip select -> SSD1331ボードのCS (Chip Select)
+   * GPIO 18 (pin 24) SCK/spi0_sclk -> SSD1331ボードのSCL
+   * GPIO 19 (pin 25) MOSI/spi0_tx -> SSD1331ボードのSDA (MOSI)
+   * GPIO 21 (pin 27) -> SSD1331ボードのRES (Reset)
+   * GPIO 22 (pin 26) -> SSD1331ボードのDC (Data/Command)
+   * 3.3V OUT (pin 36) -> SSD1331ボードのVCC
+   * GND (pin 38)  -> SSD1331ボードのGND
+
+   spi_default (SPI0) インスタンスを使用する。
+*/
+
+void calc_render_area_buflen(struct render_area *area) {
+    // レンダーエリアのバッファの長さを計算する: データは16bit
+    area->buflen = 2 * (area->end_col - area->start_col + 1) * (area->end_page - area->start_page + 1);
+}
+
+void send_cmd(spi_inst_t *spi, uint8_t cmd) {
+    gpio_put(SPI_CSN_PIN, 0);
+    gpio_put(SPI_DCN_PIN, 0);
+    spi_write_blocking(spi, &cmd, 1);
+    gpio_put(SPI_DCN_PIN, 1);
+    gpio_put(SPI_CSN_PIN, 1);
+}
+
+void send_cmd_list(spi_inst_t *spi, uint8_t *buf, size_t len) {
+    for (int i = 0; i < len; i++)
+        send_cmd(spi, buf[i]);
+}
+
+void send_data(spi_inst_t *spi, uint8_t *buf, size_t len) {
+    gpio_put(SPI_CSN_PIN, 0);
+    spi_write_blocking(spi, buf, len);
+    gpio_put(SPI_CSN_PIN, 1);
+}
+
+void reset(spi_inst_t *spi) {
+    gpio_put(SPI_RESN_PIN, 1);
+    gpio_put(SPI_CSN_PIN, 1);
+    busy_wait_ms(5);
+    gpio_put(SPI_RESN_PIN, 0);
+    busy_wait_ms(80);
+    gpio_put(SPI_RESN_PIN, 1);
+    busy_wait_ms(20);
+}
+
+void rect(spi_inst_t *spi, uint32_t x, uint32_t width, uint32_t y, uint32_t height)
+{
+    uint8_t cmds[] = {
+        SSD1331_SET_COL_ADDR, x, width,
+        SSD1331_SET_ROW_ADDR, y, height
+    };
+    send_cmd_list(spi, cmds, count_of(cmds));
+}
+
+void clear(spi_inst_t *spi, uint16_t color) {
+    uint8_t buf[] = {
+         (uint8_t)(color >> 8 & 0xFF),
+        (uint8_t)(color & 0xFF)
+    };
+
+    rect(spi, 0, SSD1331_WIDTH - 1, 0, SSD1331_HEIGHT - 1);
+
+    for (int i = 0; i < (SSD1331_WIDTH * SSD1331_HEIGHT); i++)
+        send_data(spi, buf, 2);
+}
+
+void write_pixel(spi_inst_t *spi, int x, int y, uint16_t color) {
+    uint8_t buf[] = {
+        (uint8_t)(color >> 8 & 0xFF),
+        (uint8_t)(color & 0xFF)
+    };
+    rect(spi, x, x, y, y);
+    send_data(spi, buf, 2);
+}
+
+void draw_line(spi_inst_t *spi, int x0, int y0, int x1, int y1, uint16_t color) {
+    assert(x0 >= 0 && x1 <= SSD1331_WIDTH - 1
+        && y0 >= 0 && y1 <= SSD1331_HEIGHT - 1);
+
+    int dx =  abs(x1 - x0);
+    int sx = x0 < x1 ? 1 : -1;
+    int dy = -abs(y1-y0);
+    int sy = y0 < y1 ? 1 : -1;
+    int err = dx + dy;
+    int e2;
+
+    while (true) {
+        write_pixel(spi, x0, y0, color);
+        if (x0 == x1 && y0 == y1)
+            break;
+        e2 = 2 * err;
+
+        if (e2 >= dy) {
+            err += dy;
+            x0 += sx;
+        }
+        if (e2 <= dx) {
+            err += dx;
+            y0 += sy;
+        }
+    }
+}
+
+ void ssd1331_init(spi_inst_t *spi, uint freq) {
+    // SPI0 を 10MHz で使用.
+    spi_init(spi, freq);
+
+    gpio_set_function(SPI_SCK_PIN, GPIO_FUNC_SPI);
+    gpio_set_function(SPI_MOSI_PIN, GPIO_FUNC_SPI);
+
+    // CS# (Chip select)ピンはアクティブLOWなので、HIGH状態で初期化
+    gpio_init(SPI_CSN_PIN);
+    gpio_set_function(SPI_CSN_PIN, GPIO_FUNC_SPI);
+    gpio_set_dir(SPI_CSN_PIN, GPIO_OUT);
+    gpio_put(SPI_CSN_PIN, 1);
+
+    // D/C# (Data/Command)ピンはアクティブLOWなので、HIGH状態で初期化
+    gpio_init(SPI_DCN_PIN);
+    //gpio_set_function(SPI_DCN_PIN, GPIO_FUNC_SIO);
+    gpio_set_dir(SPI_DCN_PIN, GPIO_OUT);
+    gpio_put(SPI_DCN_PIN, 1);
+
+    // RES# (Data/Command)ピンはアクティブLOWなので、HIGH状態で初期化
+    gpio_init(SPI_RESN_PIN);
+    //gpio_set_function(SPI_RESN_PIN, GPIO_FUNC_SIO);
+    gpio_set_dir(SPI_RESN_PIN, GPIO_OUT);
+    gpio_put(SPI_RESN_PIN, 1);
+
+    // ssd1331をリセット
+    reset(spi);
+
+    // デフォルト値で初期化
+    uint8_t cmds[] = {
+        SSD1331_SET_DISP_OFF,
+        SSD1331_SET_ROW_ADDR, 0x00, 0x3F,
+        SSD1331_SET_COL_ADDR, 0x00, 0x5F,
+        SSD1331_REMAP_COLOR_DEPTH, 0x72,
+        SSD1331_SET_DISP_START_LINE, 0x00,
+        SSD1331_SET_DISP_OFFSET, 0x00,
+        SSD1331_SET_NORM_DISP,
+        SSD1331_SET_MUX_RATIO, 0x3F,
+        SSD1331_SET_MASTER_CONFIG, 0xBE,
+        SSD1331_POWER_SAVE, 0x0B,
+        SSD1331_ADJUST, 0x74,
+        SSD1331_DISP_CLOck, 0xF0,
+        SSD1331_SET_PRECHARGE_A, 0x64,
+        SSD1331_SET_PRECHARGE_B, 0x78,
+        SSD1331_SET_PRECHARGE_C, 0x64,
+        SSD1331_SET_PRECHARGE_LEVEL, 0x3A,
+        SSD1331_SET_VCOMH, 0x3E,
+        SSD1331_MASTER_CURRENT_CNTL, 0x06,
+        SSD1331_SET_CONTRAST_A, 0x91,
+        SSD1331_SET_CONTRAST_B, 0x50,
+        SSD1331_SET_CONTRAST_C, 0x7D,
+        SSD1331_SET_DISP_ON_NORM
+    };
+
+    send_cmd_list(spi, cmds, count_of(cmds));
+};
+
+int main() {
+    // stdioの初期化
+    stdio_init_all();
+    // ssd1331の初期化
+    ssd1331_init(spi_default, 10 * 1000 * 1000);
+    printf("Hello, SSD1331\n");
+
+    clear(spi_default, COL_WHITE);
+    draw_line(spi_default, 10, 5, 80, 60, COL_BLACK);
+    busy_wait_ms(500);
+    draw_line(spi_default, 10, 5, 80, 60, COL_WHITE);
+    busy_wait_ms(500);
+    draw_line(spi_default, 20, 10, 50, 40, COL_BLUE);
+    busy_wait_ms(500);
+    draw_line(spi_default, 20, 10, 50, 40, COL_WHITE);
+    busy_wait_ms(500);
+    draw_line(spi_default, 0, 0, 95, 63, COL_RED);
+    busy_wait_ms(500);
+    draw_line(spi_default, 0, 0, 95, 63, COL_WHITE);
+    busy_wait_ms(500);
+    draw_line(spi_default, 0, 0, 95, 63, COL_GREEN);
+    busy_wait_ms(500);
+    clear(spi_default, COL_BLACK);
+    send_cmd(spi_default, SSD1331_SET_DISP_OFF);
+
+/*
+        busy_wait_ms(500);
+        clear(spi_default, COL_BLACK);
+        send_cmd(spi_default, SSD1331_SET_DISP_OFF);
+
+    while(1) {
+        clear(spi_default, COL_WHITE);
+        busy_wait_ms(500);
+        clear(spi_default, COL_YELLOW);
+        busy_wait_ms(500);
+        clear(spi_default, COL_RED);
+        busy_wait_ms(500);
+        clear(spi_default, COL_MAGENTA);
+        busy_wait_ms(500);
+        clear(spi_default, COL_BLUE);
+        busy_wait_ms(500);
+        clear(spi_default, COL_AQUA);
+        busy_wait_ms(500);
+        clear(spi_default, COL_GREEN);
+        busy_wait_ms(500);
+    }
+*/
+    return 0;
+}
---
+++ ssd1331.h
@@ -0,0 +1,148 @@
+/**
+ * Copyright (c) 2023 SUZUKI Keiji.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "pico/stdlib.h"
+#include "pico/binary_info.h"
+#include "hardware/spi.h"
+
+/* SPI経由で96x64 16bit-Color OLEDディスプレイを駆動するSSD1331を
+   操作するサンプルコード
+
+   * GPIO 17 (pin 22) Chip select -> SSD1331ボードのCS (Chip Select)
+   * GPIO 18 (pin 24) SCK/spi0_sclk -> SSD1331ボードのSCL
+   * GPIO 19 (pin 25) MOSI/spi0_tx -> SSD1331ボードのSDA (MOSI)
+   * GPIO 21 (pin 27) -> SSD1331ボードのRES (Reset)
+   * GPIO 20 (pin 26) -> SSD1331ボードのDC (Data/Command)
+   * 3.3V OUT (pin 36) -> SSD1331ボードのVCC
+   * GND (pin 38)  -> SSD1331ボードのGND
+
+   spi_default (SPI0) インスタンスを使用する。
+*/
+#define SPI_SCK_PIN         PICO_DEFAULT_SPI_SCK_PIN
+#define SPI_MOSI_PIN        PICO_DEFAULT_SPI_TX_PIN
+#define SPI_CSN_PIN         PICO_DEFAULT_SPI_CSN_PIN
+#define SPI_DCN_PIN         20
+#define SPI_RESN_PIN        21
+
+#define SSD1331_HEIGHT      64
+#define SSD1331_WIDTH       96
+
+#define RGB(r,g,b)	(uint16_t)(((r >> 3) << 11) | ((g >> 2) << 5) | (b >> 3))
+
+#define FrontG		COL_WHITE
+#define BackG   	COL_BLACK
+
+#define COL_BLACK	RGB(0,0,0)
+#define COL_WHITE	RGB(255,255,255)
+#define COL_RED		RGB(255,0,0)
+#define COL_GREEN	RGB(0,255,0)
+#define COL_BLUE	RGB(0,0,255)
+
+
+#define COL_YELLOW	RGB(255,255,0)
+#define COL_MAGENTA	RGB(255,0,255)
+#define COL_AQUA	RGB(0,255,255)
+
+#define COL_PURPLE	RGB(160,32,240)
+#define COL_REDPINK RGB(255,50,50)
+#define COL_ORANGE  RGB(255,165,0)
+#define	COL_LGRAY	RGB(160,160,160)
+#define	COL_GRAY	RGB(128,128,128)
+
+
+// 基本コマンド
+#define SSD1331_SET_COL_ADDR        _u(0x15)    // 桁アドレスの設定: 0x00/0x5F
+#define SSD1331_SET_ROW_ADDR        _u(0x75)    // 行アドレスの設定: 0x00/0x3F
+#define SSD1331_SET_CONTRAST_A      _u(0x81)    // カラー"A"のコントラストの設定: 0x80
+#define SSD1331_SET_CONTRAST_B      _u(0x82)    // カラー"B"のコントラストの設定: 0x80
+#define SSD1331_SET_CONTRAST_C      _u(0x83)    // カラー"C"のコントラストの設定: 0x80
+#define SSD1331_MASTER_CURRENT_CNTL _u(0x87)    // マスターカレント制御: 0x0F
+#define SSD1331_SET_PRECHARGE_A     _u(0x8A)    // カラー"A"の二次プリチャージ速度の設定: 0x81
+#define SSD1331_SET_PRECHARGE_B     _u(0x8B)    // カラー"A"の二次プリチャージ速度の設定: 0x82
+#define SSD1331_SET_PRECHARGE_C     _u(0x8C)    // カラー"A"の二次プリチャージ速度の設定: 0x83
+#define SSD1331_REMAP_COLOR_DEPTH   _u(0xA0)    // Remapとカラー震度の設定: 0x40
+#define SSD1331_SET_DISP_START_LINE _u(0xA1)    // ディスプレイ開始行の設定: 0x00
+#define SSD1331_SET_DISP_OFFSET     _u(0xA2)    // ディスプレイオフセットの設定: 0x00
+#define SSD1331_SET_NORM_DISP       _u(0xA4)
+#define SSD1331_SET_ALL_ON          _u(0xA5)
+#define SSD1331_SET_ALL_OFF         _u(0xA6)
+#define SSD1331_SET_INV_DISP        _u(0xA7)
+#define SSD1331_SET_MUX_RATIO       _u(0xA8)    // 多重度比を設定: 0x3F
+#define SSD1331_SET_DIM             _u(0xAB)    // Dimモードの設定: 0x00/A/B/C/D/プリチャージ
+#define SSD1331_SET_MASTER_CONFIG   _u(0xAD)    // マスター構成の設定: 0x8F
+#define SSD1331_SET_DISP_ON_DIM     _u(0xAC)    // DIMモードでディスプレイオン
+#define SSD1331_SET_DISP_OFF        _u(0xAE)    // ディスプレイオフ
+#define SSD1331_SET_DISP_ON_NORM    _u(0xAF)    // ノーマルモードでディスプレイオン
+#define SSD1331_POWER_SAVE          _u(0xB0)    // パワーセーブモード: 0x1A: 有効、0x0B: 無効
+#define SSD1331_ADJUST              _u(0xB1)    // フェーズ1,2クロック数の調整: 0x74
+#define SSD1331_DISP_CLOck          _u(0xB3)    // ディスプレイクロック分周比/発振周波数: 0xD0
+#define SSD1331_SET_GRAY_SCALE      _u(0xB8)    // グレイスケールテーブルの設定: 32バイト
+#define SSD1331_EN_LINEAR_SCALE     _u(0xB9)    // 線形グレイスケールテーブルの有効化
+#define SSD1331_SET_PRECHARGE_LEVEL _u(0xBB)    // プリチャージレベルの設定: 0x3E
+#define SSD1331_NOP                 _u(0xBC)    // NOP, 0xBD, 0xE3もNOP
+#define SSD1331_SET_VCOMH           _u(0xBE)    // Vcomhの設定: 0x3E
+#define SSD1331_SET_COMND_LOCK      _u(0xFD)    // コマンドロックの設定: 0x12
+
+// 描画コマンド
+#define SSD1331_DRAW_LINE          _u(0x21)     // ラインの描画: 7バイト
+#define SSD1331_DRAW_RECT          _u(0x22)     // 矩形の描画: 10バイト
+#define SSD1331_COPY               _u(0x23)     // コピー: 6バイト
+#define SSD1331_DIM_WIN            _u(0x24)     // ウィンドウをぼやかす: 4バイト
+#define SSD1331_CLEAR_WIN          _u(0x25)     // ウィンドウをクリア: 4バイト
+#define SSD1331_FILL               _u(0x26)     // 塗りつぶしの有効/無効: 0x00
+#define SSD1331_SETUP_SCROL        _u(0x27)     // 水平/垂直スクロールの設定: 5バイト
+#define SSD1331_DEACT_SCROL        _u(0x2E)     // スクロールを停止
+#define SSD1331_ACT_SCROL          _u(0x2F)     // スクロールを開始
+
+struct render_area {
+    uint8_t start_col;
+    uint8_t end_col;
+    uint8_t start_page;
+    uint8_t end_page;
+
+    int buflen;
+};
+
+static inline void cs_select(void) {
+    asm volatile("nop \n nop \n nop");
+    gpio_put(SPI_CSN_PIN, 0);  // Active low
+    asm volatile("nop \n nop \n nop");
+}
+
+static inline void cs_deselect(void) {
+    asm volatile("nop \n nop \n nop");
+    gpio_put(SPI_CSN_PIN, 1);
+    asm volatile("nop \n nop \n nop");
+}
+
+static inline void dc_command(void) {
+    //asm volatile("nop \n nop \n nop");
+    gpio_put(SPI_DCN_PIN, 0);  // command
+    //asm volatile("nop \n nop \n nop");
+}
+
+static inline void dc_data() {
+    //asm volatile("nop \n nop \n nop");
+    gpio_put(SPI_DCN_PIN, 1);   // data
+    //asm volatile("nop \n nop \n nop");
+}
+
+void calc_render_area_buflen(struct render_area *area);
+void send_cmd(spi_inst_t *spi, uint8_t cmd);
+void send_cmd_list(spi_inst_t *spi, uint8_t *buf, size_t len);
+void send_data(spi_inst_t *spi, uint8_t *buf, size_t len);
+
+void rect(spi_inst_t *spi, uint32_t x, uint32_t width, uint32_t y, uint32_t height);
+
+void reset(spi_inst_t *spi);
+void clear(spi_inst_t *spi, uint16_t color);
+void draw_line(spi_inst_t *spi, int x0, int y0, int x1, int y1, uint16_t color);
+void write_pixel(spi_inst_t *spi, int x, int y, uint16_t color);
+void ssd1331_init(spi_inst_t *spi, uint freq);
